6f8130ce1418b8448c6691104fd9009affa3aa84
fabiocostaqueiroz7-source <fabiocostaqueiroz7@gmail.com>
2026-07-16 16:33:13 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 // CONFIGURAÇÃO DO SUPABASE
-// Depois que criar seu projeto no Supabase, cole aqui sua URL e sua ANON KEY.
-window.SUPABASE_URL = "";https://jiuktsoaozwwtqwuzqno.supabase.co/rest/v1/
-window.SUPABASE_ANON_KEY = "";sb_publishable_1WIX4T3druDtXECIyhUvCQ_K9tOz-1H
-
+window.URL_SUPABASE = "https://jiuktsoaozwwtqwuzqno.supabase.co";
+window.SUPABASE_ANON_KEY = "sb_publishable_1WIX4T3druDtXECIyhUvCQ_K9tOz-1H";
